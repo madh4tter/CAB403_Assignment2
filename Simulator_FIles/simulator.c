4c3fbca169963916460e5d1f6c57cf99d3373f28
EllePlant <109197455+EllePlant@users.noreply.github.com>
2022-10-01 15:25:11 +1000

    Add files via upload

---
+++ Simulator_FIles/simulator.c
@@ -0,0 +1,20 @@
+/* Include necessary standard libraries */
+#include <stdlib.h>
+#include <stdio.h>
+#include <semaphore.h>
+
+/* Include shared memory struct definitions */
+#include "PARKING.h"
+
+
+
+int main(void){
+    char m = 20;
+    printf("%d\n", m);
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
